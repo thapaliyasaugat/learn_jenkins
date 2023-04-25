bd6c22936f495e6dc0f7a9d4790d544ea0ca9934
Sauagt Thapaliya <thapaliyasaugat99@gmail.com>
2023-04-25 16:12:28 +0545

    lombok annotation used on ErrorDetails class

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/exception/ErrorDetails.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/exception/ErrorDetails.java
@@ -1,28 +1,18 @@
 package net.guides.springboot2.springboot2jpacrudexample.exception;
 
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorDetails {
 	private Date timestamp;
 	private String message;
 	private String details;
 
-	public ErrorDetails(Date timestamp, String message, String details) {
-		super();
-		this.timestamp = timestamp;
-		this.message = message;
-		this.details = details;
-	}
-
-	public Date getTimestamp() {
-		return timestamp;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public String getDetails() {
-		return details;
-	}
 }
